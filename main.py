9d954e9a6b23520fcb18f68091f92456d7b1fb79
timwoodland <timwoodland@me.com>
2021-10-06 22:13:51 +1100

    Updated for next lecture

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import cv2
 
+img = cv2.imread("galaxy.jpg", 0)
 
-def main():
-    return
+print(type(img))
+print(img.shape)
+print(img.ndim)
 
+w = img.shape[0] / 2
+h = img.shape[1] / 2
 
+resized = cv2.resize(img, (int(h),int(w)))
 
-if __name__ == "__main__":
-    main()
+# cv2.imshow("Galaxy", resized)
+cv2.imwrite("Galaxy_resized.jpg", resized)
 
+cv2.waitKey(0)
